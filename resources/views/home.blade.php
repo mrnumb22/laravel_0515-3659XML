9dc9e74d99ce7b327faa880df3cab1f5af5b7796
Sanoj Senar <mrNumb22@gmail.com>
2019-02-18 08:31:51 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,11 @@
                     @endif
 
                     You are logged in!
+                <div id="app">
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
                 </div>
             </div>
         </div>
